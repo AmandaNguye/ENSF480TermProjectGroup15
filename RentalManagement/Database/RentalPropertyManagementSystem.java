f9d3ce4b053a9903095dfc646108c8b1813c65cb
Kaumil Patel <spdmode@gmail.com>
2021-12-05 16:47:04 -0700

    db

--- RentalManagement/Database/RentalPropertyManagementSystem.java
+++ RentalManagement/Database/RentalPropertyManagementSystem.java
@@ -1,5 +1,133 @@
 package RentalManagement.Database;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 public class RentalPropertyManagementSystem {
-    
+	private Connection dbConnect;
+//	private ResultSet results;
+	private String DBurl = "jdbc:mysql://localhost/ensf480";
+	private String username = "user";
+	private String password = "pass1234";
+
+	public RentalPropertyManagementSystem() {
+
+	}
+
+	public void initializeConnection() {
+		try {
+			dbConnect = DriverManager.getConnection(DBurl, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Failed to connect to database");
+		}
+	}
+
+	public String login(String username, String password) {
+		try {
+			String query = "SELECT * FROM USERS WHERE username = ? AND password = ?";
+			PreparedStatement myStmt = dbConnect.prepareStatement(query);
+			myStmt.setString(1, username);
+			myStmt.setString(2, password);
+			ResultSet results = myStmt.executeQuery();
+			if(!results.next()) {
+				return null;
+			}
+			return results.getString("accounttype");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public boolean registerAccount(String username, String password, String accountType) {
+		try {
+			String query = "INSERT INTO USERS(username, password, accounttype) VALUES (?, ?, ?)";
+			PreparedStatement myStmt = dbConnect.prepareStatement(query);
+			myStmt.setString(1, username);
+			myStmt.setString(2, password);
+			myStmt.setString(3, accountType);
+			myStmt.execute();
+			myStmt.close();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean registerProperty(String owner, String type, int bedrooms, int bathrooms, String furnished, String quadrant, String address) {
+		try {
+			String query = "INSERT INTO properties(owner, type, bedrooms, bathrooms, furnished, quadrant, daysleft, address, status) VALUES (?, ?, ?, ?, ?, ?,'0000-00-00', ?, 'suspended')";
+			PreparedStatement myStmt = dbConnect.prepareStatement(query);
+			myStmt.setString(1, owner);
+			myStmt.setString(2, type);
+			myStmt.setInt(3, bedrooms);
+			myStmt.setInt(4, bathrooms);
+			myStmt.setString(5, furnished);
+			myStmt.setString(6, quadrant);
+			myStmt.setString(9, address);
+			myStmt.execute();
+			myStmt.close();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean updateFeeData(int amount, int period) {
+		try {
+			String query = "set @amount = ?, @period = ?";
+			PreparedStatement myStmt = dbConnect.prepareStatement(query);
+			myStmt.setInt(1, amount);
+			myStmt.setInt(2, period);
+			myStmt.execute();
+			myStmt.close();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public ResultSet getFeeData() {
+		try {
+			String query = "select @amount, @period";
+			PreparedStatement myStmt = dbConnect.prepareStatement(query);
+			return myStmt.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public ResultSet searchPropety(String type, int bedrooms, int bathrooms, String furnished, String quadrant) {
+		try {
+			String query = "SELECT * FROM properties WHERE type = ? AND bedrooms = ? AND bathrooms = ? ANDfurnished = ? AND quadrant = ? AND daysleft > ? AND status = active";
+			PreparedStatement myStmt = dbConnect.prepareStatement(query);
+			myStmt.setString(1, type);
+			myStmt.setInt(2, bedrooms);
+			myStmt.setInt(3, bathrooms);
+			myStmt.setString(4, furnished);
+			myStmt.setString(5, quadrant);
+			myStmt.setDate(6, Date.valueOf(LocalDate.now()));
+			return myStmt.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static void main(String args[]) {
+		RentalPropertyManagementSystem rpms = new RentalPropertyManagementSystem();
+		rpms.initializeConnection();
+		System.out.println(rpms.login("landlord1", "ensf480"));
+		System.out.println(rpms.registerAccount("renter10", "ensf481", "renter"));
+	}
 }
--- database.sql
+++ database.sql
@@ -1,13 +1,17 @@
+
+
 DROP DATABASE IF EXISTS ENSF480;
 CREATE DATABASE ENSF480; 
 USE ENSF480;
 
+SET @amount = 45;
+SET @period = 1;
 
 DROP TABLE IF EXISTS USERS;
 CREATE TABLE USERS (
 	username        VARCHAR(150) NOT NULL,
     password        VARCHAR(150) NOT NULL,
-    accounttype     VARCHAR(150) NOT NULL,
+    account_type     VARCHAR(150) NOT NULL,
     PRIMARY KEY     (username)
 );
 
@@ -16,31 +20,50 @@ VALUES
     ('landlord1', 'ensf480', 'landlord'),
     ('landlord2', 'ensf480', 'landlord'),
     ('landlord3', 'ensf480', 'landlord'),
-    ('renter1', 'ensf480', 'renter'),
-    ('renter2', 'ensf480', 'renter'),
-    ('renter3', 'ensf480', 'renter'),
-    ('manager', 'ensf480', 'manager');
+    ('renter1', 'ensf490', 'renter'),
+    ('renter2', 'ensf490', 'renter'),
+    ('renter3', 'ensf490', 'renter'),
+    ('manager1', 'ensf500', 'manager'),
+    ('manager2', 'ensf500', 'manager');
 
 DROP TABLE IF EXISTS PROPERTIES;
 CREATE TABLE PROPERTIES (
 	id          INT unsigned NOT NULL AUTO_INCREMENT,
     owner       VARCHAR(150) NOT NULL,
+	address 	VARCHAR(150) NOT NULL,
     type        VARCHAR(150) NOT NULL,
     bedrooms    INT unsigned NOT NULL,
     bathrooms   INT unsigned NOT NULL,
-    furnished   VARCHAR(5) NOT NULL,
-    quadrant    VARCHAR(3) NOT NULL,
-    daysleft    INT unsigned NOT NULL,
-    occupied    VARCHAR(5) NOT NULL,
-    PRIMARY KEY (id)
+    furnished   VARCHAR(150) NOT NULL,
+    quadrant    VARCHAR(150) NOT NULL,
+    daysleft    DATE NOT NULL,
+    occupied    VARCHAR(150) NOT NULL,
+    state    	VARCHAR(150) NOT NULL,
+    PRIMARY KEY (id),
 	FOREIGN KEY (owner) REFERENCES USERS(username) ON UPDATE CASCADE
 );
 
-INSERT INTO PROPERTIES (owner, type, bedrooms, bathrooms, furnished, quadrant, daysleft, occupied)
+INSERT INTO PROPERTIES (owner, type, bedrooms, bathrooms, furnished, quadrant, daysleft, occupied, address)
 VALUES
-    ('landlord1',   'apartment',	    1,	2,	'yes',	'NE',	60, 'no'),
-    ('landlord1',   'apartment',	    1,	2,	'yes',	'NE',	60, 'no'),
-    ('landlord2',   'attached house',	1,	2,	'yes',	'NW',	60, 'no'),
-    ('landlord2',   'attached house',	1,	2,	'yes',	'NW',	60, 'no'),
-    ('landlord3',   'townhouse',	    1,	2,	'yes',	'SE',	60, 'no'),
-    ('landlord3',   'townhouse',	    1,	2,	'yes',	'SW',	60, 'no');
+    ('landlord1',   'apartment',	    1,	2,	'yes',	'NE',	60, 'no', '123 street', 'active'),
+    ('landlord1',   'apartment',	    1,	2,	'yes',	'NE',	60, 'no', '124 street', 'rented'),
+    ('landlord2',   'attached house',	1,	2,	'yes',	'NW',	60, 'no', '125 street', 'cancelled'),
+    ('landlord2',   'attached house',	1,	2,	'yes',	'NW',	60, 'no', '126 street', 'suspended'),
+    ('landlord3',   'townhouse',	    1,	2,	'yes',	'SE',	60, 'no', '127 street', 'active'),
+    ('landlord3',   'townhouse',	    1,	2,	'yes',	'SW',	60, 'no', '128 street', 'active');
+
+    
+DROP TABLE IF EXISTS MESSAGES;
+CREATE TABLE MESSAGES (
+    receiver	VARCHAR(150) NOT NULL,
+    message		VARCHAR(150) NOT NULL,
+	FOREIGN KEY (receiver) REFERENCES USERS(username) ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS SUBSCRIPTION;
+CREATE TABLE SUBSCRIPTION (
+    receiver	VARCHAR(150) NOT NULL,
+    message		VARCHAR(150) NOT NULL,
+	FOREIGN KEY (receiver) REFERENCES USERS(username) ON UPDATE CASCADE
+);
+
