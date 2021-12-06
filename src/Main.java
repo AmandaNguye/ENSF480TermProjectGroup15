134b1e2343b93499d5f0ed01fc738d19fea6180a
Amanda Nguyen <77892730+AmandaNguye@users.noreply.github.com>
2021-12-06 04:22:05 -0700

    Add testing option to Main

--- src/Main.java
+++ src/Main.java
@@ -62,16 +62,18 @@ public class Main extends JFrame {
       new Runnable() {
         public void run() {
           try {
-            Menu frame = new GuestMenu();
-            frame.setVisible(true);
-            /*
-            Menu frame1 = new LandlordMenu("moussavifan");
-            frame1.setVisible(true);
-            Menu frame2 = new RenterMenu("amanda");
-            frame2.setVisible(true);
-            Menu frame3 = new Manager("manager");
-            frame3.setVisible(true);
-            */
+            Boolean testing = false;
+            if (!testing) {
+              Menu frame = new GuestMenu();
+              frame.setVisible(true);
+            } else {
+              Menu frame1 = new LandlordMenu("moussavifan");
+              frame1.setVisible(true);
+              Menu frame2 = new RenterMenu("renter1");
+              frame2.setVisible(true);
+              Menu frame3 = new ManagerMenu("manager");
+              frame3.setVisible(true);
+            }
           } catch (Exception e) {
             e.printStackTrace();
           }
