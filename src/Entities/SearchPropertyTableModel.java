4a4f3f70a15fc9449c9abec388acaf379dc630b0
Amanda Nguyen <77892730+AmandaNguye@users.noreply.github.com>
2021-12-05 22:14:28 -0700

    Remove Owner From Search Property Table

--- src/Entities/SearchPropertyTableModel.java
+++ src/Entities/SearchPropertyTableModel.java
@@ -6,16 +6,14 @@ import javax.swing.table.AbstractTableModel;
 public class SearchPropertyTableModel extends AbstractTableModel {
 
   private static final int NAME_COL = 0;
-  private static final int OWNER_COL = 1;
-  private static final int TYPE_COL = 2;
-  private static final int BEDROOM_COL = 3;
-  private static final int BATHROOM_COL = 4;
-  private static final int FURNISHED_COL = 5;
-  private static final int QUADRANT_COL = 6;
+  private static final int TYPE_COL = 1;
+  private static final int BEDROOM_COL = 2;
+  private static final int BATHROOM_COL = 3;
+  private static final int FURNISHED_COL = 4;
+  private static final int QUADRANT_COL = 5;
 
   private String[] columnNames = {
     "Name",
-    "Owner",
     "Type",
     "Bedrooms",
     "Bathrooms",
@@ -50,8 +48,6 @@ public class SearchPropertyTableModel extends AbstractTableModel {
     switch (col) {
       case NAME_COL:
         return tempProperty.getName();
-      case OWNER_COL:
-        return tempProperty.getOwner();
       case TYPE_COL:
         return tempProperty.getType();
       case BEDROOM_COL:
